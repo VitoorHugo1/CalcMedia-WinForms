62e7ad5f2b48b0d0355886579413a8784b60a7f9
dev_vitorh <103538149+VitoorHugo1@users.noreply.github.com>
2023-11-22 18:24:45 -0300

    Update Form1.cs

--- CalcMedia/CalcMedia/Form1.cs
+++ CalcMedia/CalcMedia/Form1.cs
@@ -26,32 +26,26 @@ namespace CalcMedia
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
-
         }
 
         private void label4_Click(object sender, EventArgs e)
         {
-
         }
 
         private void label2_Click(object sender, EventArgs e)
         {
-
         }
 
         private void label5_Click(object sender, EventArgs e)
         {
-
         }
 
         private void label3_Click(object sender, EventArgs e)
         {
-
         }
 
         private void button1_Click(object sender, EventArgs e)// Calcular
