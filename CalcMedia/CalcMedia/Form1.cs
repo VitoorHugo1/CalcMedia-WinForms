bc0b92c2410b0aadbf8c92acd4a0bb6b730e33e9
Vitor Hugo Mateus <103538149+VitoorHugo1@users.noreply.github.com>
2024-01-26 12:36:40 -0300

    Update Form1.cs
    removing unused method

--- CalcMedia/CalcMedia/Form1.cs
+++ CalcMedia/CalcMedia/Form1.cs
@@ -19,10 +19,6 @@ namespace CalcMedia
             InitializeComponent();
         }
 
-        private void label3_Click(object sender, EventArgs e)
-        {
-        }
-
         private void button1_Click(object sender, EventArgs e)// Calcular
         {
             float soma, media, valor;
